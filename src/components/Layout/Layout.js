c1a9fc8495371194d0c094960275d4b5e5ac5a94
jeffreychen <jeffreychen2016@gmail.com>
2019-04-15 22:34:42 -0500

    feat: addtional setup

---
+++ src/components/Layout/Layout.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Layout.css';
+
+const Layout = (props) => {
+    return (
+        <div className="Layout">
+            <h1>Layout component</h1>
+        </div>
+    );
+};
+
+export default Layout
